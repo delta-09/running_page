042f24f2748570b4126163ba36e3728abb733df0
delta-09 <623667953@qq.com>
2026-04-06 13:56:44 +0800

    Update const.ts

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -55,7 +55,7 @@ const IS_CHINESE = true;
 const USE_ANIMATION_FOR_GRID = false;
 const CHINESE_INFO_MESSAGE = (yearLength: number, year: string): string => {
   const yearStr = year === 'Total' ? '所有' : ` ${year} `;
-  return `我想在地图上更多的留下我的足迹，下面展示的是${yearStr}的数据`;
+  return `我想在地图上留下更多的足迹，下面展示的是${yearStr}的数据。`;
 };
 const ENGLISH_INFO_MESSAGE = (yearLength: number, year: string): string =>
   `Running Journey with ${yearLength} Years, the table shows year ${year} data`;
